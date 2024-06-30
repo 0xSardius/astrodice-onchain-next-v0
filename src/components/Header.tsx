a56f727cf98dc60e7f43d33535b484bf8fbd8b97
Sardius <0xsardius@gmail.com>
2024-06-30 09:30:21 -0400

    Header fixed, looking gud

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,24 +6,20 @@ import { chain } from "../app/chain";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mb-20 md:mb-20">
+    <header className="flex flex-col items-center justify-center m-5">
       <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        onchain-Astrodice
-        {/* <span className="text-zinc-300 inline-block mx-1"> + </span>
-        <span className="inline-block -skew-x-6 text-blue-500"> Next.js </span> */}
+        onchain-astrodice
       </h1>
 
-      <p className="text-zinc-300 text-base">
-        <div>
-          {/* Could upgrade this eventually with a Connect embed*/}
-          <ConnectButton client={client} chain={chain} />
-        </div>
-        Read the{" "}
+      <div>
+        {/* Could upgrade this eventually with a Connect embed*/}
+        <ConnectButton client={client} chain={chain} />
+      </div>
+      {/* Read the{" "}
         <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
           README.md
         </code>{" "}
-        file to get started.
-      </p>
+        file to get started. */}
     </header>
   );
 }
